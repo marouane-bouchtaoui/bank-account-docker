a56c3e19a29ef8d390599f05c37a24b916ea5066
marouane-bouchtaoui <mrnb778@gmail.com>
2023-12-22 15:36:10 +0100

    v-dk

---
+++ angular-front-end/src/app/customers/customers.component.ts
@@ -0,0 +1,32 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {NgForOf, NgIf} from "@angular/common";
+
+@Component({
+  selector: 'app-customers',
+  standalone: true,
+  imports: [
+    HttpClientModule,
+    NgForOf,
+    NgIf
+  ],
+  templateUrl: './customers.component.html',
+  styleUrl: './customers.component.css'
+})
+export class CustomersComponent implements OnInit{
+  customers : any;
+  constructor(private http: HttpClient) {
+  }
+  ngOnInit() {
+    this.http.get("http://localhost:8888/CUSTOMER-SERVICE/customers")
+      .subscribe({
+        next : data => {
+          this.customers = data;
+        },
+        error : err => {
+          console.log(err);
+        }
+      })
+  }
+
+}
